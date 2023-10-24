42c69c518e4da24c2574bf38e18f63c3cf601df0
PatrickLyonsISD <patricklyonslit@gmail.com>
2023-10-24 13:20:44 +0100

    put back in player turn at the top and added a winning message

--- app/src/main/java/com/example/Tapatan/ui/GameScreen.kt
+++ app/src/main/java/com/example/Tapatan/ui/GameScreen.kt
@@ -1,53 +1,58 @@
-
 package com.example.Tapatan.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.Tapatan.R
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GameScreen(gameViewModel: GameViewModel = viewModel()) {
-    val currentPlayer = gameViewModel.currentPlayer
+    val currentPlayer by gameViewModel.currentPlayer
     val buttons = gameViewModel.buttons
+    val gameEnded by gameViewModel.gameEnded
+    val winner by gameViewModel.winner
+
+
+
+    if (gameEnded && winner != null) {
+        AlertDialog(
+            onDismissRequest = { gameViewModel.resetGame() },
+            title = { Text("Congratulations!") },
+            text = { Text("$winner player, you won!") },
+            confirmButton = {
+                Button(onClick = { gameViewModel.resetGame() }) {
+                    Text("OK")
+                }
+            }
+        )
+    }
+
 
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 16.dp),
+            modifier = Modifier.fillMaxWidth(),
             contentAlignment = Alignment.TopCenter
         ) {
-
+            Text(
+                text = "Turn: $currentPlayer",
+                fontSize = 40.sp
+            )
         }
 
 
@@ -63,7 +68,7 @@ fun GameScreen(gameViewModel: GameViewModel = viewModel()) {
                 ) {
                     for (col in 0 until 3) {
                         MyButton(
-                            text = buttons[row][col].value,
+                            text = buttons[row][col] ?: "",
                             onClick = {
                                 gameViewModel.onButtonClick(row, col)
                             }
@@ -79,56 +84,20 @@ fun GameScreen(gameViewModel: GameViewModel = viewModel()) {
     }
 }
 
-@Composable
-fun TopBar() {
-    Scaffold(
-        topBar = {
-            TopAppBar()
-        }
-    ) { it ->
-        LazyColumn(contentPadding = it) {
-
-            }
-        }
-    }
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TopAppBar(modifier: Modifier = Modifier) {
-    CenterAlignedTopAppBar(
-        title = {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-
-
-                Text(
-                    text = stringResource(R.string.app_name),
-                    style = MaterialTheme.typography.displayLarge
-
-                )
-            }
-        },
-        modifier = modifier
-    )
-}
 
 
 @Composable
-fun MyButton(text: String?, onClick: () -> Unit) {
+fun MyButton(text: String, onClick: () -> Unit) {
     Button(
-        onClick = { onClick() },
+        onClick = onClick,
         modifier = Modifier.padding(8.dp)
     ) {
-        Text(text ?: "")
+        Text(text)
     }
 }
-
-
 
 @Preview
 @Composable
 fun MyComposableLayoutPreview() {
     GameScreen(GameViewModel())
-    TopAppBar()
 }
--- app/src/main/java/com/example/Tapatan/ui/GameViewModel.kt
+++ app/src/main/java/com/example/Tapatan/ui/GameViewModel.kt
@@ -1,42 +1,29 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.Tapatan.ui
 
-import androidx.compose.runtime.MutableState
+
+
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 
 
 class GameViewModel : ViewModel() {
-    var buttons: Array<Array<MutableState<String?>>> = Array(3) { Array(3) { mutableStateOf(null) } }
-    var currentPlayer: MutableState<String> = mutableStateOf("Black")
-    var roundCount: MutableState<Int> = mutableStateOf(0)
-    var gameEnded: MutableState<Boolean> = mutableStateOf(false)
+    var buttons = Array(3) { arrayOfNulls<String>(3) }
+    var currentPlayer = mutableStateOf("Black")
+    var roundCount = 0
+    var gameEnded = mutableStateOf(false)
+    var winner = mutableStateOf<String?>(null)
 
 
 
     fun onButtonClick(row: Int, col: Int) {
-        if (buttons[row][col].value == null && !gameEnded.value) {
-            buttons[row][col].value = currentPlayer.value
-            roundCount.value = roundCount.value + 1
+        if (buttons[row][col] == null && !gameEnded.value) {
+            buttons[row][col] = currentPlayer.value
+            roundCount++
             if (checkForWin(row, col)) {
                 gameEnded.value = true
-            } else if (roundCount.value == 9) {
+                winner.value = currentPlayer.value
+            } else if (roundCount == 9) {
                 gameEnded.value = true
             } else {
                 currentPlayer.value = if (currentPlayer.value == "Black") "White" else "Black"
@@ -47,13 +34,13 @@ class GameViewModel : ViewModel() {
 
 
     private fun checkForWin(row: Int, col: Int): Boolean {
-        val player = buttons[row][col].value
+        val player = buttons[row][col]
 
 
 
         // Check for horizontal win
         for (i in 0 until 3) {
-            if (buttons[row][i].value != player) {
+            if (buttons[row][i] != player) {
                 break
             }
             if (i == 2) {
@@ -65,7 +52,7 @@ class GameViewModel : ViewModel() {
 
         // Check for vertical win
         for (i in 0 until 3) {
-            if (buttons[i][col].value != player) {
+            if (buttons[i][col] != player) {
                 break
             }
             if (i == 2) {
@@ -78,7 +65,7 @@ class GameViewModel : ViewModel() {
         // Check for diagonal win (top-left to bottom-right)
         if (row == col) {
             for (i in 0 until 3) {
-                if (buttons[i][i].value != player) {
+                if (buttons[i][i] != player) {
                     break
                 }
                 if (i == 2) {
@@ -92,7 +79,7 @@ class GameViewModel : ViewModel() {
         // Check for diagonal win (top-right to bottom-left)
         if (row + col == 2) {
             for (i in 0 until 3) {
-                if (buttons[i][2 - i].value != player) {
+                if (buttons[i][2 - i] != player) {
                     break
                 }
                 if (i == 2) {
@@ -109,13 +96,10 @@ class GameViewModel : ViewModel() {
 
 
     fun resetGame() {
-        for (i in 0..2) {
-            for (j in 0..2) {
-                buttons[i][j].value = null
-            }
-        }
+        buttons = Array(3) { arrayOfNulls<String>(3) }
         currentPlayer.value = "Black"
-        roundCount.value = 0
+        roundCount = 0
         gameEnded.value = false
+        winner.value = null
     }
 }
