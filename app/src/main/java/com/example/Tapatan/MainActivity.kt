dfdd321341558a4ff028e85fd49e199606e82cba
PatrickLyonsISD <patricklyonslit@gmail.com>
2023-10-24 12:42:04 +0100

    Added a banner thing thats not showing on the phone but is there in the preview

--- app/src/main/java/com/example/Tapatan/MainActivity.kt
+++ app/src/main/java/com/example/Tapatan/MainActivity.kt
@@ -19,10 +19,22 @@ package com.example.Tapatan
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import com.example.Tapatan.ui.GameScreen
 import com.example.Tapatan.ui.theme.UnscrambleTheme
 
@@ -42,3 +54,5 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
--- app/src/main/java/com/example/Tapatan/ui/GameScreen.kt
+++ app/src/main/java/com/example/Tapatan/ui/GameScreen.kt
@@ -8,23 +8,33 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.Tapatan.R
 
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GameScreen(gameViewModel: GameViewModel = viewModel()) {
     val currentPlayer = gameViewModel.currentPlayer
     val buttons = gameViewModel.buttons
-
 
 
     Column(
@@ -69,6 +79,39 @@ fun GameScreen(gameViewModel: GameViewModel = viewModel()) {
     }
 }
 
+@Composable
+fun TopBar() {
+    Scaffold(
+        topBar = {
+            TopAppBar()
+        }
+    ) { it ->
+        LazyColumn(contentPadding = it) {
+
+            }
+        }
+    }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+
+                )
+            }
+        },
+        modifier = modifier
+    )
+}
 
 
 @Composable
@@ -87,4 +130,5 @@ fun MyButton(text: String?, onClick: () -> Unit) {
 @Composable
 fun MyComposableLayoutPreview() {
     GameScreen(GameViewModel())
+    TopAppBar()
 }
